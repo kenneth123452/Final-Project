0877dfd6d7106af7d68bf90aa99ce0b493e6405c
JayarM1618 <151903474+JayarM1618@users.noreply.github.com>
2023-12-01 11:07:02 +0800

    Update Goaljs.js

--- Goaljs.js
+++ Goaljs.js
@@ -5,15 +5,10 @@ function calculateRecommendedSpending() {
 
     const formattedGoalAmount = goalAmount.toLocaleString('en-PH', { style: 'currency', currency: 'PHP' });
 
-    const resultDiv = document.getElementById('result');
+    /* const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = `
         <p>Your goal of ${formattedGoalAmount} in ${timeFrame} has been set.</p>
-    `;
-    const suggestions = getSuggestions(); // Replace this with your actual function or API call
-
-      // Display suggestions
-    displaySuggestions(suggestions);
-    
+    `;*/
     if (retrievedRemainingAllowance !== null) {
         console.log("Retrieved Remaining Allowance: ₱" + retrievedRemainingAllowance);
 
@@ -26,6 +21,10 @@ function calculateRecommendedSpending() {
         // Update the progress bar and text
         updateProgressBar(percentageCompletion);
       }
+     const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `
+        <p>Your goal of ${formattedGoalAmount} in ${timeFrame} has been set.</p>
+    `;
 }
 
     function updateProgressBar(percentage) {
@@ -34,26 +33,4 @@ function calculateRecommendedSpending() {
 
       progressBar.style.width = `${percentage}%`;
       progressText.innerText = `Progress: ${percentage.toFixed(2)}%`;
-    }
-
-    function getSuggestions() {
-      // For demonstration purposes, using a simple array
-      return [
-        "Consider reducing dining out expenses.",
-        "Explore cost-effective subscription plans.",
-        "Look for additional income sources.",
-        // Add more suggestions as needed
-      ];
-    }
-
-    // Function to display suggestions
-    function displaySuggestions(suggestions) {
-      const suggestionList = document.getElementById('suggestion-list');
-      suggestionList.innerHTML = ''; // Clear existing suggestions
-
-      suggestions.forEach((suggestion) => {
-        const li = document.createElement('li');
-        li.textContent = suggestion;
-        suggestionList.appendChild(li);
-      });
     }
