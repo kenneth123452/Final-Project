bd817800f87b846a187730d21f22b2cb0d1dfb66
JayarM1618 <151903474+JayarM1618@users.noreply.github.com>
2023-12-01 14:07:27 +0800

    Update Goaljs.js

--- Goaljs.js
+++ Goaljs.js
@@ -2,14 +2,12 @@ function setGoal() {
     const goalAmount = parseFloat(document.getElementById('goal-amount').value);
     const timeFrame = document.getElementById('time-frame').value;
     const formattedGoalAmount = goalAmount.toLocaleString('en-PH', { style: 'currency', currency: 'PHP' });
-
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `<p>Your goal of ${formattedGoalAmount} in ${timeFrame} has been set.</p>`;
+    console.log(`Your goal of ${formattedGoalAmount} in ${timeFrame} has been set.`);
     
     updateProgressAndChart(goalAmount);
 }
 
-function calculateRecommendedSpending() {
+function updateProgressAndChart(goalAmount) {
     /*const goalAmount = parseFloat(document.getElementById('goal-amount').value);
     const timeFrame = document.getElementById('time-frame').value;*/
     const retrievedRemainingAllowance = localStorage.getItem("Remaining Allowance");
@@ -83,6 +81,7 @@ function calculateRecommendedSpending() {
             options: options
         });
     }
+}
         /*function redirectToAnotherPage() {
     window.location.href = "Daily.html";
 }
