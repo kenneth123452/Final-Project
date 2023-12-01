d1738aa600d18e376b702b67604366c6c5def4c5
JayarM1618 <151903474+JayarM1618@users.noreply.github.com>
2023-12-01 14:21:07 +0800

    Update Goaljs.js

--- Goaljs.js
+++ Goaljs.js
@@ -1,47 +1,35 @@
 function setGoal() {
     const goalAmount = parseFloat(document.getElementById('goal-amount').value);
     const timeFrame = document.getElementById('time-frame').value;
-    const formattedGoalAmount = goalAmount.toLocaleString('en-PH', { style: 'currency', currency: 'PHP' });
-    console.log(`Your goal of ${formattedGoalAmount} in ${timeFrame} has been set.`);
+    console.log(`Your goal of ${goalAmount} in ${timeFrame} has been set.`);
     
     updateProgressAndChart(goalAmount);
 }
 
 function updateProgressAndChart(goalAmount) {
-    /*const goalAmount = parseFloat(document.getElementById('goal-amount').value);
-    const timeFrame = document.getElementById('time-frame').value;*/
     const retrievedRemainingAllowance = localStorage.getItem("Remaining Allowance");
-
-    /*const formattedGoalAmount = goalAmount.toLocaleString('en-PH', { style: 'currency', currency: 'PHP' });
-   
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `
-        <p>Your goal of ${formattedGoalAmount} in ${timeFrame} has been set.</p>
-    `;*/
     
     if (retrievedRemainingAllowance !== null) {
         console.log("Retrieved Remaining Allowance: ₱" + retrievedRemainingAllowance);
-        // Do something with the retrievedRemainingAllowance, such as displaying it on the page
         document.getElementById("displayRemainingAllowance").innerText = "Remaining Allowance: ₱" + retrievedRemainingAllowance;
-        // Calculate percentage completion
+
         const percentageCompletion = (retrievedRemainingAllowance / goalAmount) * 100;
-        console.log("Percentage Completion: " + percentageCompletion); // Debugging line
-        // Update the progress bar and text
+        console.log("Percentage Completion: " + percentageCompletion);
+        
         updateProgressBar(percentageCompletion);
         createOrUpdateBarChart(retrievedRemainingAllowance, goalAmount);
     }
 }
 
+function updateProgressBar(percentage) {
+    const progressBar = document.getElementById('progressBar');
+    const progressText = document.getElementById('progressText');
 
-    function updateProgressBar(percentage) {
-      const progressBar = document.getElementById('progressBar');
-      const progressText = document.getElementById('progressText');
+    progressBar.style.width = `${percentage}%`;
+    progressText.innerText = `Progress: ${percentage.toFixed(2)}%`;
+}
 
-      progressBar.style.width = `${percentage}%`;
-      progressText.innerText = `Progress: ${percentage.toFixed(2)}%`;
-    }
-
-    function createOrUpdateBarChart(remainingAllowance, goalAmount) {
+function createOrUpdateBarChart(remainingAllowance, goalAmount) {
     const ctx = document.getElementById('barChart').getContext('2d');
     const data = {
         labels: ['Remaining Allowance', 'Goal'],
@@ -68,13 +56,10 @@ function updateProgressAndChart(goalAmount) {
         }
     };
 
-    // Check if the chart already exists
     if (window.myBarChart) {
-        // Update existing chart
         window.myBarChart.data = data;
         window.myBarChart.update();
     } else {
-        // Create a new chart
         window.myBarChart = new Chart(ctx, {
             type: 'bar',
             data: data,
@@ -82,11 +67,4 @@ function updateProgressAndChart(goalAmount) {
         });
     }
 }
-        /*function redirectToAnotherPage() {
-    window.location.href = "Daily.html";
-}
-
-function goBack() {
-    window.location.href = "Expenses.html";
-}*/
 
