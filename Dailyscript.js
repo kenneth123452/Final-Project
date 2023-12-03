8dae3296578dee690f2a0f70f80b13384b4b3a3c
JayarM1618 <151903474+JayarM1618@users.noreply.github.com>
2023-12-03 22:47:47 +0800

    Update Dailyscript.js

--- Dailyscript.js
+++ Dailyscript.js
@@ -11,100 +11,82 @@ if (storedTimeFrame !== null) {
 }
 
 const renderCalendar = () => {
-  date.setDate(1);
+    date.setDate(1);
 
-  const monthDays = document.querySelector(".calendar-days-content");
-  
-  const monthDays = document.querySelector(".calendar-days-content");
-  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-  const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    const monthDays = document.querySelector(".calendar-days-content");
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 
-  const firstDayIndex = date.getDay();
-(const monthDays = document.querySelector(".calendar-days-content");
+    const firstDayIndex = date.getDay();
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
 
-    const lastDay = new Date(
-        date.getFullYear(),
-        date.getMonth() + 1,
-        0
-    ).getDate();
+    document.getElementById("monthDisplay").innerHTML = months[date.getMonth()];
+    document.getElementById("yearDisplay").innerHTML = date.getFullYear();
 
-    const prevLastDay = new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        0
-    ).getDate();
+    let days = "";
 
-    const firstDayIndex = date.getDay();)
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  document.getElementById("monthDisplay").innerHTML = months[date.getMonth()];
-  document.getElementById("yearDisplay").innerHTML = date.getFullYear();
-
-  let days = "";
-
-  for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
-  }
-  for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div class="today">${i}</div>`;
-    } else {
-      days += `<div class="day" data-day="${i}">${i}</div>`;
+    for (let x = firstDayIndex; x > 0; x--) {
+        days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
     }
-  }
 
-  monthDays.innerHTML = days;
-  document.querySelectorAll(".day").forEach((day) => {
-    day.addEventListener("click", handleDayClick);
-  });
+    for (let i = 1; i <= lastDay; i++) {
+        if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+            days += `<div class="today">${i}</div>`;
+        } else {
+            days += `<div class="day" data-day="${i}">${i}</div>`;
+        }
+    }
+
+    monthDays.innerHTML = days;
+    document.querySelectorAll(".day").forEach((day) => {
+        day.addEventListener("click", handleDayClick);
+    });
 };
 
 document.getElementById("prevMonthButton").addEventListener("click", () => {
-  date.setMonth(date.getMonth() - 1);
-  renderCalendar();
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
 });
 
 document.getElementById("nextMonthButton").addEventListener("click", () => {
-  date.setMonth(date.getMonth() + 1);
-  renderCalendar(); 
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
 });
+
 const openSchedulingModal = (day) => {
-  document.getElementById("selectedDay").innerHTML = day;
-  document.getElementById("schedulingModal").style.display = "block";
+    document.getElementById("selectedDay").innerHTML = day;
+    document.getElementById("schedulingModal").style.display = "block";
 };
 
 // Add this function to handle day clicks
 const handleDayClick = (event) => {
-  const selectedDay = event.currentTarget.dataset.day;
-  openSchedulingModal(selectedDay);
+    const selectedDay = event.currentTarget.dataset.day;
+    openSchedulingModal(selectedDay);
 };
 
 // Add this function to handle form submission
 const handleFormSubmit = (event) => {
-  event.preventDefault();
-  const eventData = {
-    title: document.getElementById("eventTitle").value,
-    time: document.getElementById("eventTime").value,
-    description: document.getElementById("eventDescription").value,
-  };
+    event.preventDefault();
+    const eventData = {
+        title: document.getElementById("eventTitle").value,
+        time: document.getElementById("eventTime").value,
+        description: document.getElementById("eventDescription").value,
+    };
 
-  const storedTimeFrame = localStorage.getItem('timeFrame');
+    const storedTimeFrame = localStorage.getItem('timeFrame');
 
     // Check if the time frame is available and not null
     if (storedTimeFrame !== null) {
@@ -138,12 +120,16 @@ const calculateScheduleTime = (timeFrame) => {
     const formattedTime = nextOccurrenceDate.toLocaleString('en-US', { weekday: 'long', hour: 'numeric', minute: 'numeric', hour12: true });
     return formattedTime;
 };
-  
+
 // Add event listener for form submission
 document.getElementById("eventForm").addEventListener("submit", handleFormSubmit);
 
 // Add event listener to close modal
 document.getElementById("closeModalButton").addEventListener("click", () => {
-  document.getElementById("schedulingModal").style.display = "none";
+    document.getElementById("schedulingModal").style.display = "none";
 });
+
+// Initial rendering of the calendar
+renderCalendar();
+
 
