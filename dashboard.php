edef81c1ddbaa34a08875c79b7dc5746a861b04f
JayarM1618 <151903474+JayarM1618@users.noreply.github.com>
2023-12-04 22:33:07 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,20 +1,45 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.html");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>User Dashboard</title>
+    <link rel="stylesheet" href="styless.css">
 </head>
 <body>
-    <h2>Login</h2>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required>
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" name="password" required>
-        <br>
-        <input type="submit" value="Login">
-    </form>
+    <div class="banner">
+        <div class="navbar">
+            <img src="src/img/WiseBudgeLogo.png" class="logo">
+            <ul>
+                <li><a href="Allowance.html">Allowance</a></li>
+                <li><a href="Expenses.html">Expenses</a></li>
+                <li><a href="Goal.html">Goal</a></li>
+                <li><a href="Daily.html">Daily</a></li>
+            </ul>
+        </div>
+        <div class="Budgeting">
+            <h1>Budgeting Financial Management</h1>
+        </div>
+        <div class="content">
+            <p>Welcome, <?php echo $_SESSION['username']; ?>!</p>
+        </div>
+        <div class="pees">
+            <p>Plan.</p>
+            <p>Budget.</p>
+            <p>Save.</p>
+        </div>
+    </div>
+    <footer>
+        <p class="footer">"2023 WiseBudge. All rights reserved."</p>
+    </footer>
 </body>
 </html>
