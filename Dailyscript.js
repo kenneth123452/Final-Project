c9d1ca5f8187bd8afcb9fa0fe4c98ec9dc3a9b8f
kenneth123452 <151898679+kenneth123452@users.noreply.github.com>
2023-12-10 19:26:57 +0800

    Update Dailyscript.js

--- Dailyscript.js
+++ Dailyscript.js
@@ -1,193 +1,70 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const date = new Date();
-    const storedTimeFrame = localStorage.getItem('timeFrame');
-    const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const date = new Date();
 
-    const getTargetDay = (timeFrame) => {
-        const currentDate = new Date();
-        const targetDate = new Date(timeFrame);
+const renderCalendar = () => {
+  date.setDate(1);
 
-        if (targetDate < currentDate) {
-            let nextOccurrenceDate = currentDate;
-            while (nextOccurrenceDate.toLocaleDateString('en-US', { weekday: 'long' }) !== timeFrame) {
-                nextOccurrenceDate.setDate(nextOccurrenceDate.getDate() + 1);
-            }
-            return nextOccurrenceDate.getDate();
-        } else {
-            return targetDate.getDate();
-        }
-    };
+  const monthDays = document.querySelector(".calendar-days-content");
 
-    const determineTimeFrameType = (timeFrame) => {
-        if (timeFrame.length === 3) {
-            return "week";
-        } else if (timeFrame.length > 3 && timeFrame.includes(" ")) {
-            return "month";
-         } else if (timeFrame.length > 3 && !timeFrame.includes(" ")) {
-            return "day";
-        } else {
-            
-            return "year";
-        }
-    };
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
 
-    const calculateDaysRemaining = (timeFrameType, timeFrame) => {
-        const currentDate = new Date();
-        const daysInWeek = 7;
-        const daysInMonth = 30;
-        const daysInYear = 365;
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
 
-    if (timeFrameType === "week") {
-        const targetDayIndex = weekDays.indexOf(timeFrame);
-        const currentDayIndex = currentDate.getDay();
-        return (targetDayIndex + 7 - currentDayIndex) % 7;
-    } else if (timeFrameType === "month") {
-        return daysInMonth - currentDate.getDate();
-    } else if (timeFrameType === "year") {
-        return daysInYear - getDayInYear(timeFrame);
+  const firstDayIndex = date.getDay();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.getElementById("monthDisplay").innerHTML = months[date.getMonth()];
+  document.getElementById("yearDisplay").innerHTML = date.getFullYear();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
     } else {
-        return 0; // Invalid time frame type
+      days += `<div>${i}</div>`;
     }
+  }
 
-    };
+  monthDays.innerHTML = days;
+};
 
-    const getDayInYear = (timeFrame) => {
-        const months = [
-            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
-        ];
-
-        const monthDay = timeFrame.split(" ");
-        const month = months.indexOf(monthDay[0]);
-        const day = parseInt(monthDay[1]);
-
-        let dayInYear = day;
-        for (let i = 0; i < month; i++) {
-            dayInYear += new Date(date.getFullYear(), i + 1, 0).getDate();
-        }
-
-        return dayInYear;
-    };
-
-    const renderCalendar = () => {
-        date.setDate(1);
-
-        const monthDays = document.querySelector(".calendar-days-content");
-        const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-        const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-
-        const firstDayIndex = date.getDay();
-        const months = [
-            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
-        ];
-
-        const currentDate = new Date();
-        const currentDay = currentDate.getDate();
-        const currentMonth = currentDate.getMonth();
-        const currentYear = currentDate.getFullYear();
-
-        const retrievedTimeFrameDay = getTargetDay(storedTimeFrame);
-        const timeFrameType = determineTimeFrameType(storedTimeFrame);
-        const daysRemaining = calculateDaysRemaining(timeFrameType, storedTimeFrame);
-
-        document.getElementById("monthDisplay").innerHTML = months[date.getMonth()];
-        document.getElementById("yearDisplay").innerHTML = date.getFullYear();
-
-        let days = "";
-
-    for (let x = firstDayIndex; x > 0; x--) {
-        days += `<div class="prev-date"><span class="math-inline">${prevLastDay - x + 1}</span></div>`;
-    }
-
-    for (let i = 1; i <= lastDay; i++) {
-        let dayClass = "day";
-        let timeFrameDisplay = "";
-
-        if (i === currentDay && date.getMonth() === currentMonth) {
-            dayClass = "today";
-        }
-
-        if (i === retrievedTimeFrameDay) {
-            dayClass += " retrieved-time-frame";
-            timeFrameDisplay = `<div class="time-frame">${storedTimeFrame}</div>`;
-        }
-        const currentDateInLoop = new Date(date.getFullYear(), date.getMonth(), i);
-        const formattedCurrentDate = currentDateInLoop.toLocaleDateString('en-US', { weekday: 'long' });
-        if (formattedCurrentDate === storedTimeFrame) {
-            dayClass += " marked-date";
-        }
-
-        days += `<div class="${dayClass}" data-day="${i}">
-                    ${i}
-                    ${timeFrameDisplay}
-                </div>`;
-    }
-        monthDays.innerHTML = days;
-        document.querySelectorAll(".day").forEach((day) => {
-            day.addEventListener("click", handleDayClick);
-        });
-
-        document.getElementById("daysRemaining").innerHTML = `Days remaining: ${daysRemaining}`;
-    };
-
-    const handleDayClick = (event) => {
-        const selectedDay = event.currentTarget.dataset.day;
-        openSchedulingModal(selectedDay);
-    };
-
-    const openSchedulingModal = (day) => {
-        document.getElementById("selectedDay").innerHTML = day;
-        document.getElementById("schedulingModal").style.display = "block";
-    };
-
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        const eventData = {
-            title: document.getElementById("eventTitle").value,
-            time: document.getElementById("eventDay").value,
-            description: document.getElementById("eventDescription").value,
-        };
-
-        const storedTimeFrame = localStorage.getItem('timeFrame');
-
-        if (storedTimeFrame !== null) {
-            console.log('Retrieved Time Frame:', storedTimeFrame);
-
-    const scheduleTime = calculateScheduleTime(storedTimeFrame);
-            eventData.time = scheduleTime;
-
-            console.log("Event Scheduled:", eventData);
-            document.getElementById("schedulingModal").style.display = "none";
-        } else {
-            console.log('Time Frame not found in localStorage.');
-        }
-    };
-
-    const calculateScheduleTime = (timeFrame) => {
-        const currentDate = new Date();
-        let nextOccurrenceDate = currentDate;
-
-        while (nextOccurrenceDate.toLocaleDateString('en-US', { weekday: 'long' }) !== timeFrame) {
-            nextOccurrenceDate.setDate(nextOccurrenceDate.getDate() + 1);
-        }
-
-        return nextOccurrenceDate; 
-    };
-
-    document.getElementById("eventForm").addEventListener("submit", handleFormSubmit);
-
-    document.getElementById("closeModalButton").addEventListener("click", () => {
-        document.getElementById("schedulingModal").style.display = "none";
-    });
-
-    document.getElementById("prevMonthButton").addEventListener("click", () => {
-        date.setMonth(date.getMonth() - 1);
-        renderCalendar();
-    });
-
-    document.getElementById("nextMonthButton").addEventListener("click", () => {
-        date.setMonth(date.getMonth() + 1);
-        renderCalendar();
-    });
-
-    renderCalendar();
+document.getElementById("prevMonthButton").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
 });
+
+document.getElementById("nextMonthButton").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar(); 
+});
+
+renderCalendar();
