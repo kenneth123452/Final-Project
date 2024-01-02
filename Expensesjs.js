92da4ef8149d0284d0274bd664339d4040ec6eec
JayarM1618 <151903474+JayarM1618@users.noreply.github.com>
2024-01-02 23:40:46 +0800

    Update Expensesjs.js

--- Expensesjs.js
+++ Expensesjs.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
             displayStoredDataElement.innerText =
                 "Stored Allowance Type: " + allowanceType + "\n" +
                 "Stored Allowance Amount: " + allowanceValue;
+        }
     }
 
     // Submit form function
