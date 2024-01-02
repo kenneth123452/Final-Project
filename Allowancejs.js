a486e9ca28d39e4a67d16ee825def6dedfb72f2d
JayarM1618 <151903474+JayarM1618@users.noreply.github.com>
2024-01-02 21:05:00 +0800

    Update Allowancejs.js

--- Allowancejs.js
+++ Allowancejs.js
@@ -1,10 +1,15 @@
 function submitForm() {
+    function submitForm() {
     var allowanceValue = document.getElementById("value").value;
+    var allowanceValue2= document.getElementById("value").value;
+    var allowanceValue3= document.getElementById("value").value;
     var dataToStore = {
         "Weekly Allowance": allowanceValue
+        "Daily Allowance" : allowanceValue2
+        "Monthly Allowance" : allowanceValue3
     };
     var jsonString = JSON.stringify(dataToStore);
-    localStorage.setItem("Weekly Allowance", jsonString);
+    localStorage.setItem("DataAllowance", jsonString);
 
     //document.getElementById("displayValue").innerText = "Entered Value: " + allowanceValue;
     var index = "Expenses.html";
@@ -12,10 +17,13 @@ function submitForm() {
 }
 
 function goBack() {
-    var storedData = localStorage.getItem("Weekly Allowance");
+    var storedData = localStorage.getItem("DataAllowance");
     if (storedData) {
         var parsedData = JSON.parse(storedData);
-        document.getElementById("displayValue").innerText = "Stored Value: " + parsedData["Weekly Allowance"];
+        document.getElementById("displayValue").innerText = 
+            "Stored Value: " + parsedData["Weekly Allowance"];
+            "Stored Value: " + parsedData["Daily Allowance"];
+            "Stored Value: " + parsedData["Monthly Allowance"];
     }
 
     window.history.back();
